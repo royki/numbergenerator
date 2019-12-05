52df48ed1ee21cadafd9c4800423507e84a88919
roy <royki@users.noreply.github.com>
2019-12-05 23:40:58 +0100

    small change

--- src/main/scala/generator/Generator.scala
+++ src/main/scala/generator/Generator.scala
@@ -50,11 +50,12 @@ object Generator extends App {
     
     // val generatorA_16 = Produce.leastSignificatBits_16(Produce.A, Produce.factorA, Produce.process_amount)
     // val generatorB_16 = Produce.leastSignificatBits_16(Produce.B, Produce.factorB, Produce.process_amount)
-    val all_matching_pairs = generatorA.zip(generatorB).filter(i => (i._1 & 0x0000FFFFL) == (i._2 & 0x0000FFFFL))
-    all_matching_pairs.foreach(println)
-    
     val number_of_matching_pairs = generatorA.zip(generatorB).count(i => (i._1 & 0x0000FFFFL) == (i._2 & 0x0000FFFFL))
     println(s"$number_of_matching_pairs Number of Matching Pairs are found from 2 random number generators that process ${Produce.process_amount} number")
+    
+    val all_matching_pairs = generatorA.zip(generatorB).filter(i => (i._1 & 0x0000FFFFL) == (i._2 & 0x0000FFFFL))
+    all_matching_pairs.foreach(println)
+   
 }
 
    
